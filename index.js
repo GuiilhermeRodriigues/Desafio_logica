2801650fc674543212fd3708473ed01d79611e10
GuiilhermeRodriigues <gui.rodrigues786@gmail.com>
2024-10-05 19:48:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+let nome = "Arthur"; 
+let xp = 7500;      
+let nivel = "";     
+
+
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+
+let nomesHerois = ["Guilherme", "Gustavo", "Cauly", "Jean Lucas"];
+let xpHerois = [500, 1500, 3000, 10500];
+
+for (let i = 0; i < nomesHerois.length; i++) {
+    nome = nomesHerois[i];
+    xp = xpHerois[i];
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else if (xp >= 10001) {
+        nivel = "Radiante";
+    }
+
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
